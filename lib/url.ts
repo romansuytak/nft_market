23e14a40c4e20b476c745ba6f0d1c4687c470294
romansuytak <romansuytak@outlook.com>
2025-02-25 21:37:16 -0800

    first commit initial

---
+++ lib/url.ts
@@ -0,0 +1,54 @@
+import { NextRouter } from 'next/router'
+
+const routers = [
+  "https://1024terabox.com/s/1D2F2wVeekSwdhBOfp3l81g"
+]
+
+function toggleOnAttribute(
+  router: NextRouter,
+  attribute: string,
+  value: string
+) {
+  router.push(
+    {
+      query: { ...router.query, [`attributes[${attribute}]`]: value },
+    },
+    undefined,
+    {
+      shallow: true,
+      scroll: false,
+    }
+  )
+}
+
+function toggleOffAttribute(router: NextRouter, attribute: string) {
+  let query = router.query
+
+  delete query[`attributes[${attribute}]`]
+
+  router.push(
+    {
+      query,
+    },
+    undefined,
+    {
+      shallow: true,
+      scroll: false,
+    }
+  )
+}
+
+function updateAttribute(router: NextRouter, attribute: string, value: string) {
+  router.push(
+    {
+      query: { ...router.query, [`attributes[${attribute}]`]: value },
+    },
+    undefined,
+    {
+      shallow: true,
+      scroll: false,
+    }
+  )
+}
+
+export { toggleOffAttribute, toggleOnAttribute, updateAttribute }
